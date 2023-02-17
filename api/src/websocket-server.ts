cf10d5a854e6e0266ef0fcbe29a6bf5ba95ca796
Adam Black <adam.black@rafflebox.ca>
2023-02-17 08:52:24 -0400

    fix websocket

--- api/src/websocket-server.ts
+++ api/src/websocket-server.ts
@@ -10,7 +10,7 @@ const server = createServer({
   key: readFileSync('/home/ubuntu/key.pem'),
 });
 
-const wss = new WebSocketServer({ server, port: config.websocketPort });
+const wss = new WebSocketServer({ server });
 const clientService = new ClientService();
 
 wss.on('connection', function connection(ws) {
