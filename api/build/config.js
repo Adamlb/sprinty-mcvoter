b8aa9b9b272d536db8b2218b2bfcb55116042d29
Adam Black <adam.black@rafflebox.ca>
2023-02-17 08:50:20 -0400

    remove build from repo

--- api/build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.config = void 0;
-exports.config = {
-    websocketPort: Number(process.env.WEBSOCKET_PORT),
-    httpServerPort: Number(process.env.HTTP_SERVER_PORT)
-};
--- api/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1["default"].config();
-require("./http-server");
-require("./websocket-server");
