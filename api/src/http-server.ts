aae37ba52ea45a879ad9c0661028fbdcf91916ec
Adam Black <adam.black@rafflebox.ca>
2023-02-18 04:57:34 -0400

    mebe

--- api/src/http-server.ts
+++ api/src/http-server.ts
@@ -1,15 +1,27 @@
 import express from 'express';
+import { createServer } from 'https';
+import { readFileSync } from 'fs';
+
 import { config } from './config';
 
 const app = express();
 const port = config.httpServerPort;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const privateKey = readFileSync('/home/ubuntu/key.pem');
+const certificate = readFileSync('/home/ubuntu/cert.pem');
+
+createServer(
+  {
+    key: privateKey,
+    cert: certificate,
+  },
+  app
+).listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
 
-app.listen(port, () => {
-  console.log(`HTTP listening on port ${port}`);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 export { app };
