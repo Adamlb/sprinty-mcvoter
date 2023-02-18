2120757d89914f18c0f240ef7ce3571c0cbfb0a4
Adam Black <adam.black@rafflebox.ca>
2023-02-18 05:21:42 -0400

    remove all ssl

--- api/src/http-server.ts
+++ api/src/http-server.ts
@@ -1,22 +1,11 @@
 import express from 'express';
-import { createServer } from 'https';
-import { readFileSync } from 'fs';
 
 import { config } from './config';
 
 const app = express();
 const port = config.httpServerPort;
 
-const privateKey = readFileSync('/home/ubuntu/key.pem');
-const certificate = readFileSync('/home/ubuntu/cert.pem');
-
-createServer(
-  {
-    key: privateKey,
-    cert: certificate,
-  },
-  app
-).listen(port, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
--- api/src/websocket-server.ts
+++ api/src/websocket-server.ts
@@ -1,23 +1,14 @@
 import { WebSocketServer } from 'ws';
-import { readFileSync } from 'fs';
-import { createServer } from 'https';
 
 import { config } from './config';
 import { ClientService } from './client-service';
 
-const server = createServer({
-  cert: readFileSync('/home/ubuntu/cert.pem'),
-  key: readFileSync('/home/ubuntu/key.pem'),
-});
-
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({ port: config.websocketPort });
 const clientService = new ClientService();
 
 wss.on('connection', function connection(ws) {
   clientService.registerClient(ws);
   ws.on('error', console.error);
 });
-
-server.listen(config.websocketPort);
 
 console.log(`Websocket server listening on port ${config.websocketPort}`);
