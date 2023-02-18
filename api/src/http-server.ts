43313a14525db91e8f97aa1a090fe7d98b2307ff
Adam Black <adam.black@rafflebox.ca>
2023-02-18 05:37:33 -0400

    asdfadf

--- api/src/http-server.ts
+++ api/src/http-server.ts
@@ -9,6 +9,10 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+app.listen(80, () => {
+  console.log(`App listening on 80`);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
