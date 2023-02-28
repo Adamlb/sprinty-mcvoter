63e7f6832923e3e257de3806a778f5135323ce7c
Adam Black <adam.black@rafflebox.ca>
2023-02-28 19:36:01 -0400

    fix local api

--- api/src/websocket-server.ts
+++ api/src/websocket-server.ts
@@ -5,23 +5,28 @@ import { createServer, ServerOptions } from 'https';
 import { config } from './config';
 import { ClientService } from './client-service';
 
-const serverOptions: ServerOptions = {};
+let wss: WebSocketServer;
 
 if (config.certificate) {
+  const serverOptions: ServerOptions = {};
+
   serverOptions.cert = readFileSync(config.certificate.cert);
   serverOptions.key = readFileSync(config.certificate.key);
+
+  const server = createServer(serverOptions);
+
+  wss = new WebSocketServer({ server });
+
+  server.listen(config.websocketPort);
+} else {
+  wss = new WebSocketServer({ port: config.websocketPort });
 }
 
-const server = createServer(serverOptions);
-
-const wss = new WebSocketServer({ server });
 const clientService = new ClientService();
 
 wss.on('connection', function connection(ws) {
   clientService.registerClient(ws);
   ws.on('error', console.error);
 });
-
-server.listen(config.websocketPort);
 
 console.log(`Websocket server listening on port ${config.websocketPort}`);
