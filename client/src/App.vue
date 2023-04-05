0a87c4fa8256c80a405b3c2a0ac5ca2fcf758a94
Adam4ever <adam4ever@gmail.com>
2023-04-05 07:21:29 -0300

    Layout refactor and style touch up (#9)
    This is just me playing around with copilot and updating some layout and
    CSS stuff.
    Everything should work the same. It is still not responsive as that
    requires a bit more time than I have.
    Feel free to use or not. All good.
    
    - Added google font
    - Removed restrictive css definitions. Tried to fix some of the layout
    collisions
    - Reorganized grid layout and repurposed naming convention to allow for
    up to 8 users (didn't test with 9 so not sure what happens)
    - Added pulse animation to "Join us" button
    - Got rid of weird light mode css
    - made borders chonky
    
    <img width="620" alt="Screenshot 2023-03-28 201728"
    src="https://user-images.githubusercontent.com/5279306/228389226-c49672cd-db54-4aa0-aa90-8f0a3a080b26.png">
    
    <img width="620" alt="Screenshot 2023-03-28 201712"
    src="https://user-images.githubusercontent.com/5279306/228389256-2fa176c0-4cf9-4eaf-b141-ea745efaf161.png">

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import JoinRoom from './components/JoinRoom.vue';
 import Room from './components/Room.vue';
-import logo from './assets/beard-sprint.png';
 </script>
 
 <template>
